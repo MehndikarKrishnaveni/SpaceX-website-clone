ab01ac36fa884a8894a707f828906be09c5549e6
MehndikarKrishnaveni <krishnavenikrissh@gmail.com>
2024-10-20 14:37:21 +0530

    cloned spaceX website

---
+++ Tshirt.js
@@ -0,0 +1,15 @@
+let quanbtn1 = document.getElementById("quan-btn1")
+console.log(quanbtn1);
+quanbtn1.addEventListener("click",()=>{
+    if(quanbtn1=="-"){
+        document.getElementById("values")
+    }
+    console.log(quanbtn1);
+})
+
+let quanbtn2 = document.getElementById("quan-btn2")
+console.log(quanbtn2);
+quanbtn2.addEventListener("click", ()=>{
+    console.log(quanbtn2);
+})
+
